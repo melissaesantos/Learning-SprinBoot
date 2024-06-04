c3de8f6125952110a3441f6bd6c2c5bdd6bb6b49
Melissa Santos <melissasantos2202@gmail.com>
2024-06-04 14:50:41 -0700

    trying

---
+++ src/main/java/com/melissa/Main.java
@@ -0,0 +1,13 @@
+package com.melissa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    // inorder for this to be a spring boot application we have to add a couple of things
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
